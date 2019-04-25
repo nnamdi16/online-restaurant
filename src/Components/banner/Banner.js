a58581f0c85380c1ffd7b56e12b22e4eee4fdbf3
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-25 10:06:43 +0100

    Create  the banner component

---
+++ src/Components/banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../css/landing-page.css';
+import '../../img/banner-Image.jpg';
+const Banner = () => {
+	return (
+		<div>
+			<div className="banner-Image">
+				<p>Cool Taste, Quality.</p>
+			</div>
+			<h1>Cool Taste, Quality.</h1>
+		</div>
+	);
+};
+
+export default Banner;
