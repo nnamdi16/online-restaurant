5fe7ac856d58debca9aac93ae77572c75d8816aa
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-26 10:36:59 +0100

    Create an inline style for Banner.js

--- src/Components/banner/Banner.js
+++ src/Components/banner/Banner.js
@@ -2,12 +2,17 @@ import React from 'react';
 import '../../css/landing-page.css';
 import '../../img/banner-Image.jpg';
 const Banner = () => {
+	const divStyle = {
+		margin: '0px'
+	};
 	return (
 		<div>
 			<div className="banner-Image">
 				<p>Cool Taste, Quality.</p>
 			</div>
-			<h1>Cool Taste, Quality.</h1>
+			<div class="ui segment" style={divStyle}>
+				<h1>Cool Taste, Quality.</h1>
+			</div>
 		</div>
 	);
 };
