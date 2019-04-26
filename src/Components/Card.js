a1e107b5146f242590a8525cbf134443a89cbef3
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-26 10:36:23 +0100

    Refactor Card.js, adding an animated button, inserting image url and importing necessary files

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import '../css/landing-page.css';
+import { Button, Icon } from 'semantic-ui-react';
+import 'materialize-css/dist/css/materialize.min.css';
 
 const Cards = props => {
 	console.log(props.items);
 
 	return (
-		<div>
-			{props.items.map(food => {
-				return (
-					<div className="col s12 m4 l3 card-single">
-						<div className="card">
-							<div className="card-image">
-								<img
-									className="card-layout"
-									src={food.image_url}
-									// src="http://static.food2fork.com/Buffalo2BChicken2BGrilled2BCheese2BSandwich2B5002B4983f2702fe4.jpg"
-									alt="food recipes"
-								/>
-								<span className="card-title">{food.title}</span>
-							</div>
+		<div className="menu-display card-render">
+			<div className="row">
+				{props.items.map(food => {
+					return (
+						<div className="col s12 m4 l3 card-single">
+							<div className="card">
+								<div className="card-image">
+									<img className="card-layout" src={food.image_url} alt="food recipes" />
+									<span className="card-title">{food.title}</span>
+								</div>
 
-							<div className="card-content">
-								<p>{food.publisher}</p>
-							</div>
-							<div className="card-action">
-								<a href="#">This is a link</a>
+								<div className="card-content">
+									<p>{food.publisher}</p>
+								</div>
+								<div className="card-action">
+									<Button animated>
+										<Button.Content visible>View</Button.Content>
+										<Button.Content hidden>
+											<Icon name="arrow right" />
+										</Button.Content>
+									</Button>
+									{/* <a href="#">This is a link</a> */}
+								</div>
 							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+			</div>
 		</div>
 	);
 };
