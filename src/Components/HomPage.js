a5c84cd317f651555902b80fcd428015d20196a1
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-28 16:26:15 +0100

    Create a Homepage router that houses the Banner and Card Component

---
+++ src/Components/HomPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './banner/Banner';
+import Cards from './Card';
+
+export default () => {
+	return (
+		<div>
+			<Banner />
+			<Cards />
+		</div>
+	);
+};
