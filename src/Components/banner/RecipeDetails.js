5baf061e4cad0d63552e1e0e1d872b8537784a38
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-29 21:16:28 +0100

    Create a componentDidMount lifecycle to get a single product based on its id and create the card, adding contents

--- src/Components/banner/RecipeDetails.js
+++ src/Components/banner/RecipeDetails.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { createRecipes } from '../../actions';
+import '../../css/singleRecipe.css';
+import { Link } from 'react-router-dom';
+import { Grid, Button, Icon } from 'semantic-ui-react';
 
-const RecipeDetails = ({ recipe = '' }) => {
-	if (!recipe) {
-		return recipe;
+class RecipeDetails extends Component {
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		this.props.createRecipes(id);
 	}
-	return (
-		<div>
-			<h1>{recipe.title}</h1>
-			<h4>{recipe.publisher}</h4>
-			<h4>{recipe.social_rank} Reviews</h4>
-			<h4>Price:${recipe.id}</h4>
-			<p>{recipe.description}</p>
-		</div>
-	);
+
+	render() {
+		const { display } = this.props;
+		if (!display) {
+			return <div>Loading</div>;
+		}
+		console.log(display);
+		return (
+			<div className="ui container singleCard">
+				<Grid centered columns={2}>
+					<Grid.Column width={2}>
+						<div className="card">
+							<div className="card-image">
+								<img className="card-layout" src={display.image_url} alt="food recipes" />
+								<span className="card-title">{display.title}</span>
+							</div>
+
+							<div className="card-content">
+								<p>{display.publisher}</p>
+							</div>
+							<div className="card-action">
+								<Link className="ui animated button" to={`/posts/${display.id}`}>
+									<Button animated>
+										<Button.Content visible>View</Button.Content>
+										<Button.Content hidden>
+											<Icon name="arrow right" />
+										</Button.Content>
+									</Button>
+								</Link>
+							</div>
+						</div>
+					</Grid.Column>
+				</Grid>
+				{/* <div className="ui container">
+					<p>Recipe Shows!{display.id}</p>
+				</div> */}
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = ({ display }, ownProps) => {
+	console.log(display);
+	return { display: display.recipe };
 };
 
-const mapStateToProps = (state, ownProps) => {
-	return { recipe: state.selectedRecipe };
-};
-
-export default connect(mapStateToProps)(RecipeDetails);
+export default connect(mapStateToProps, { createRecipes })(RecipeDetails);
