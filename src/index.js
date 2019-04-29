71881b8b9677cb78713a501408ce0f4bc68053e8
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-04-29 21:10:02 +0100

    Add redux- promise to the store

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise';
 import './index.css';
 import App from './App';
 import reducers from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, promise)));
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
